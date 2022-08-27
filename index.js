17e8b2d7b02d6a51dec8bc36ea71fee02d9fb7a0
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-08-28 00:49:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    let myArray = []
+    for (let i=0; i< names.length; i++) {
+        myArray.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+   return myArray;
+
+}
+function countDown(tally) {
+    while (tally >= 0) {
+        console.log(tally--);
+    }
+}
+
+
+   
+ 
